af43339f7470c1a60da1ace44da5b3bda8c71215
yangtau <yanggtau@gmail.com>
2023-12-06 21:40:09 +0800

    fix: remove unused configs

--- clash/update_config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-dir=$HOME/.config/clash
-
-mv -f $dir/config.yaml $dir/config.yaml.bak
-wget -O $dir/config.yaml $(cat url)
-sed -i "/external/a external-ui: '${dir}/dashboard'" $dir/config.yaml
