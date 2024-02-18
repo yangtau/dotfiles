fe53804d1eaac675e0e2ca29e33d6264cf22c1fd
yangtau <yanggtau@gmail.com>
2024-02-18 20:39:12 +0800

    More LSP config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,37 @@
 vim.opt.termguicolors = true
-vim.opt.background = "light"
+vim.opt.background = 'light'
 vim.opt.relativenumber = true
 vim.opt.number = true
-vim.opt.signcolumn = "yes"
-vim.opt.fileencodings="utf-8,gb18030,gbk,gb2312"
-vim.opt.mouse=""
+vim.opt.signcolumn = 'yes'
+vim.opt.fileencodings = 'utf-8,gb18030,gbk,gb2312'
+vim.opt.mouse = ''
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 vim.opt.breakindent = true
 
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 30
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = "\\"
+vim.g.mapleader = '\\'
 
-vim.keymap.set('n', '<space>b', '<cmd>buffers<cr>:buffer<Space>')
-
-vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
+require('lazy').setup({
   {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
     init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
@@ -47,42 +43,41 @@ require("lazy").setup({
     }
   },
   {
-    "norcalli/nvim-colorizer.lua",
+    'norcalli/nvim-colorizer.lua',
     lazy = false,
     config = function()
       require('colorizer').setup { '*', }
     end
   },
   {
-    "ojroques/nvim-osc52",
+    'ojroques/nvim-osc52',
     lazy = false,
     config = function()
-      ---- osc copy over ssh ----
       local function copy(lines, _)
         require('osc52').copy(table.concat(lines, '\n'))
       end
 
       local function paste()
-        return {vim.fn.split(vim.fn.getreg(""), '\n'), vim.fn.getregtype("")}
+        return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
       end
 
       vim.g.clipboard = {
         name = 'osc52',
-        copy = {['+'] = copy, ['*'] = copy},
-        paste = {['+'] = paste, ['*'] = paste},
+        copy = { ['+'] = copy, ['*'] = copy },
+        paste = { ['+'] = paste, ['*'] = paste },
       }
 
       -- Now the '+' register will copy to system clipboard using OSC52
-      vim.keymap.set('n', '<leader>c', '"+y')
-      vim.keymap.set('n', '<leader>cc', '"+yy')
-      ---- end ----
+      -- TODO: why use these key mappings?
+      vim.keymap.set('n', '<leader>c', '\'+y')
+      vim.keymap.set('n', '<leader>cc', '\'+yy')
     end
   },
   {
-    "lewis6991/gitsigns.nvim",
+    'lewis6991/gitsigns.nvim',
     lazy = false,
     opts = {
-      signs = {
+      signs                        = {
         add          = { text = '+' },
         change       = { text = '~' },
         delete       = { text = '_' },
@@ -90,17 +85,17 @@ require("lazy").setup({
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
       },
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      watch_gitdir = {
+      signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir                 = {
         follow_files = true
       },
-      auto_attach = true,
-      attach_to_untracked = false,
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
+      auto_attach                  = true,
+      attach_to_untracked          = false,
+      current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts      = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
         delay = 1000,
@@ -109,11 +104,11 @@ require("lazy").setup({
       },
       -- TODO: put blame info in status line
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-      sign_priority = 6,
-      update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000, -- Disable if file is longer than this (in lines)
-      preview_config = {
+      sign_priority                = 6,
+      update_debounce              = 100,
+      status_formatter             = nil,   -- Use default
+      max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+      preview_config               = {
         -- Options passed to nvim_open_win
         border = 'single',
         style = 'minimal',
@@ -125,42 +120,34 @@ require("lazy").setup({
   },
   {
     'neovim/nvim-lspconfig',
-    -- "williamboman/mason.nvim",
+    -- 'williamboman/mason.nvim',
   },
   {
-    "christoomey/vim-tmux-navigator",
+    'christoomey/vim-tmux-navigator',
     lazy = false,
   },
   {
-    "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
     dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+      'L3MON4D3/LuaSnip',
     },
     config = function()
     end,
   },
   {
-    "nvim-tree/nvim-tree.lua",
+    'nvim-tree/nvim-tree.lua',
     lazy = false,
     opts = {
       renderer = {
-        indent_markers = { enable = true},
-        icons = {
-            show = {
-                -- file = false,
-                -- folder = false,
-                -- git = false,
-                -- folder_arrow = false,
-                -- modified = false,
-            },
-        },
+        indent_markers = { enable = true },
       },
-      on_attach = function (bufnr)
+      on_attach = function(bufnr)
         local api = require('nvim-tree.api')
 
         local function opts(desc)
@@ -174,131 +161,131 @@ require("lazy").setup({
         -- vim.keymap.del('n', 's', { buffer = bufnr })
         -- vim.keymap.del('n', '<C-k>', { buffer = bufnr })
         -- vim.keymap.set('n', '<C-t>', api.node.open.tab,         opts('Open: New Tab'))
-        vim.keymap.set('n', 'C', api.tree.change_root_to_node,  opts('CD'))
-        vim.keymap.set('n', 's', api.node.open.vertical,        opts('Open: Vertical Split'))
-        vim.keymap.set('n', 'a', api.fs.create,                 opts('Create'))
+        vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+        vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
+        vim.keymap.set('n', 'a', api.fs.create, opts('Create'))
       end,
     },
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    'catppuccin/nvim',
+    name = 'catppuccin',
     priority = 1000,
     lazy = false,
     init = function()
-      vim.cmd.colorscheme "catppuccin"
+      vim.cmd.colorscheme 'catppuccin'
     end,
     opts = {
-      flavour = "latte", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-          light = "latte",
-          dark = "mocha",
+      flavour = 'latte', -- latte, frappe, macchiato, mocha
+      background = {     -- :h background
+        light = 'latte',
+        dark = 'mocha',
       },
       transparent_background = false, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+      term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
+        enabled = false,              -- dims the background color of inactive window
       },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      no_underline = false, -- Force no underline
-      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = { "bold" },
-          operators = {},
+      no_italic = false,              -- Force no italic
+      no_bold = false,                -- Force no bold
+      no_underline = false,           -- Force no underline
+      styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { 'italic' },      -- Change the style of comments
+        conditionals = { 'italic' },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
       },
       color_overrides = {
-          latte = {
-              rosewater = "#cc7983",
-              flamingo = "#bb5d60",
-              pink = "#d54597",
-              mauve = "#a65fd5",
-              red = "#b7242f",
-              maroon = "#db3e68",
-              peach = "#e46f2a",
-              yellow = "#bc8705",
-              green = "#1a8e32",
-              teal = "#00a390",
-              sky = "#089ec0",
-              sapphire = "#0ea0a0",
-              blue = "#017bca",
-              lavender = "#855497",
-              text = "#444444",
-              subtext1 = "#555555",
-              subtext0 = "#666666",
-              overlay2 = "#777777",
-              overlay1 = "#888888",
-              overlay0 = "#999999",
-              surface2 = "#aaaaaa",
-              surface1 = "#bbbbbb",
-              surface0 = "#cccccc",
-              base = "#fffffd",
-              mantle = "#eeeeee",
-              crust = "#dddddd",
-          },
+        latte = {
+          rosewater = '#cc7983',
+          flamingo = '#bb5d60',
+          pink = '#d54597',
+          mauve = '#a65fd5',
+          red = '#b7242f',
+          maroon = '#db3e68',
+          peach = '#e46f2a',
+          yellow = '#bc8705',
+          green = '#1a8e32',
+          teal = '#00a390',
+          sky = '#089ec0',
+          sapphire = '#0ea0a0',
+          blue = '#017bca',
+          lavender = '#855497',
+          text = '#444444',
+          subtext1 = '#555555',
+          subtext0 = '#666666',
+          overlay2 = '#777777',
+          overlay1 = '#888888',
+          overlay0 = '#999999',
+          surface2 = '#aaaaaa',
+          surface1 = '#bbbbbb',
+          surface0 = '#cccccc',
+          base = '#ffffff',
+          mantle = '#eeeeee',
+          crust = '#dddddd',
+        },
       },
       custom_highlights = {},
       integrations = {
-          cmp = true,
-          nvimtree = false,
-          treesitter = true,
-          gitsigns = true,
-          which_key = true,
-          -- notify = false,
-          -- mini = {
-          --     enabled = true,
-          --     indentscope_color = "",
-          -- },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-                errors = { "italic" },
-                hints = { "italic" },
-                warnings = { "italic" },
-                information = { "italic" },
-            },
-            underlines = {
-                errors = { "underline" },
-                hints = { "underline" },
-                warnings = { "underline" },
-                information = { "underline" },
-            },
-            inlay_hints = {
-                background = true,
-            },
+        cmp = true,
+        nvimtree = false,
+        treesitter = true,
+        gitsigns = true,
+        which_key = true,
+        -- notify = false,
+        -- mini = {
+        --     enabled = true,
+        --     indentscope_color = '',
+        -- },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { 'italic' },
+            hints = { 'italic' },
+            warnings = { 'italic' },
+            information = { 'italic' },
           },
+          underlines = {
+            errors = { 'underline' },
+            hints = { 'underline' },
+            warnings = { 'underline' },
+            information = { 'underline' },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
       },
     },
   },
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
       local opts = {
         ensure_installed = {
-          "vim", "vimdoc", "lua", "nix",
-          "go", "gosum", "gomod",
-          "c",  "query", "rust", "bash",
-          "html", "markdown",
+          'vim', 'vimdoc', 'lua', 'nix',
+          'go', 'gosum', 'gomod',
+          'c', 'query', 'rust', 'bash',
+          'html', 'markdown',
         },
         sync_install = false,
 
         highlight = {
           enable = true,
           custom_captures = {
-            -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
-            -- ["foo.bar"] = "Identifier",
+            -- Highlight the @foo.bar capture group with the 'Identifier' highlight group.
+            -- ['foo.bar'] = 'Identifier',
           },
           -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
           -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -310,10 +297,10 @@ require("lazy").setup({
         incremental_selection = {
           enable = false,
           keymaps = {
-            init_selection = "gnn",
-            scope_incremental = "grn",
-            node_decremental = "grc",
-            node_incremental = "grm",
+            init_selection = 'gnn',
+            scope_incremental = 'grn',
+            node_decremental = 'grc',
+            node_incremental = 'grm',
           },
         },
       }
@@ -323,3 +310,4 @@ require("lazy").setup({
 })
 
 require('lsp')
+require('keymap')
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,45 +1,143 @@
-local lspconfig = require('lspconfig')
-
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function()
-    local bufmap = function(mode, lhs, rhs)
-      local opts = {buffer = true}
-      vim.keymap.set(mode, lhs, rhs, opts)
-    end
-
-    -- Displays hover information about the symbol under the cursor
-    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-    -- Jump to the definition
-    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-    -- Jump to declaration
-    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-    -- Lists all the implementations for the symbol under the cursor
-    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-    -- Jumps to the definition of the type symbol
-    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-    -- Lists all the references
-    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-    -- Displays a function's signature information
-    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-    -- Renames all references to the symbol under the cursor
-    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-    -- Selects a code action available at the current cursor position
-    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-    -- Show diagnostics in a floating window
-    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-    -- Move to the previous diagnostic
-    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-    -- Move to the next diagnostic
-    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-  end
+-- looks batter
+vim.diagnostic.config({
+  float = {
+    border = 'rounded',
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '✘',
+      [vim.diagnostic.severity.WARN] = '▲',
+      [vim.diagnostic.severity.HINT] = '⚑',
+      [vim.diagnostic.severity.INFO] = '»',
+    },
+  },
 })
 
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = 'rounded' }
+)
+
+-- highlight symbol under cursor
+vim.opt.updatetime = 400
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'Setup highlight symbol',
+  callback = function(event)
+    local id = vim.tbl_get(event, 'data', 'client_id')
+    local client = id and vim.lsp.get_client_by_id(id)
+    if client == nil or not client.supports_method('textDocument/documentHighlight') then
+      return
+    end
+
+    local group = vim.api.nvim_create_augroup('highlight_symbol', { clear = false })
+
+    vim.api.nvim_clear_autocmds({ buffer = event.buf, group = group })
+
+    vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      group = group,
+      buffer = event.buf,
+      callback = vim.lsp.buf.document_highlight,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      group = group,
+      buffer = event.buf,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end,
+})
+
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   desc = 'Enable inlay hints',
+--   callback = function(event)
+--     local id = vim.tbl_get(event, 'data', 'client_id')
+--     local client = id and vim.lsp.get_client_by_id(id)
+--     print(id, client)
+--     if client == nil or not client.supports_method('textDocument/inlayHint') then
+--       return
+--     end
+--
+--     -- warning: this api is not stable yet
+--     vim.lsp.inlay_hint.enable(event.buf, true)
+--   end,
+-- })
+
+
+---- cmp config ----
+local lspconfig = require('lspconfig')
+local luasnip = require('luasnip') -- luasnip setup
+local cmp = require('cmp')         -- nvim-cmp setup
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Up
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+  },
+}
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lspconfig.lua_ls.setup({
+  capabilities = capabilities,
   on_init = function(client)
     local path = client.workspace_folders[1].name
-    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
           runtime = {
@@ -52,11 +150,11 @@ lspconfig.lua_ls.setup({
             checkThirdParty = false,
             library = {
               vim.env.VIMRUNTIME
-              -- "${3rd}/luv/library"
-              -- "${3rd}/busted/library",
+              -- '${3rd}/luv/library'
+              -- '${3rd}/busted/library',
             }
             -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-            -- library = vim.api.nvim_get_runtime_file("", true)
+            -- library = vim.api.nvim_get_runtime_file('', true)
           }
         }
       })
@@ -66,15 +164,26 @@ lspconfig.lua_ls.setup({
 })
 
 lspconfig.nil_ls.setup {
+  capabilities = capabilities,
   autostart = true,
   settings = {
     ['nil'] = {
       formatting = {
-        command = { "nixpkgs-fmt" },
+        command = { 'nixpkgs-fmt' },
       },
     },
   },
 }
 
 lspconfig.gopls.setup {
+  capabilities = capabilities,
+  settings = {
+    ['gopls'] = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      -- gofumpt = true,
+    },
+  },
 }
