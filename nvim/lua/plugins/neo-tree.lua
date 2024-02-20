c677ce7348a2062f3f4b07d5eace38327643a6ed
yangtau <yanggtau@gmail.com>
2024-02-20 14:34:40 +0800

    Update neo-tree for nvim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -10,17 +10,7 @@ return {
     return {
       auto_clean_after_session_restore = true,
       close_if_last_window = true,
-      sources = { "filesystem", "buffers", "git_status" },
-      source_selector = {
-        winbar = true,
-        content_layout = "center",
-        sources = {
-          { source = "filesystem", display_name = get_icon("FolderClosed", 1, true) .. "File" },
-          { source = "buffers", display_name = get_icon("DefaultFile", 1, true) .. "Bufs" },
-          { source = "git_status", display_name = get_icon("Git", 1, true) .. "Git" },
-          { source = "diagnostics", display_name = get_icon("Diagnostic", 1, true) .. "Diagnostic" },
-        },
-      },
+      sources = { "filesystem" },
       default_component_configs = {
         indent = { padding = 0 },
         icon = {
@@ -46,10 +36,6 @@ return {
         },
       },
       commands = {
-        system_open = function(state)
-          -- TODO: just use vim.ui.open when dropping support for Neovim <0.10
-          (vim.ui.open or require("astronvim.utils").system_open)(state.tree:get_node():get_id())
-        end,
         parent_or_close = function(state)
           local node = state.tree:get_node()
           if (node.type == "directory" or node:has_children()) and node:is_expanded() then
@@ -70,39 +56,6 @@ return {
             state.commands.open(state)
           end
         end,
-        copy_selector = function(state)
-          local node = state.tree:get_node()
-          local filepath = node:get_id()
-          local filename = node.name
-          local modify = vim.fn.fnamemodify
-
-          local vals = {
-            ["BASENAME"] = modify(filename, ":r"),
-            ["EXTENSION"] = modify(filename, ":e"),
-            ["FILENAME"] = filename,
-            ["PATH (CWD)"] = modify(filepath, ":."),
-            ["PATH (HOME)"] = modify(filepath, ":~"),
-            ["PATH"] = filepath,
-            ["URI"] = vim.uri_from_fname(filepath),
-          }
-
-          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
-          if vim.tbl_isempty(options) then
-            utils.notify("No values to copy", vim.log.levels.WARN)
-            return
-          end
-          table.sort(options)
-          vim.ui.select(options, {
-            prompt = "Choose to copy to clipboard:",
-            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
-          }, function(choice)
-            local result = vals[choice]
-            if result then
-              utils.notify(("Copied: `%s`"):format(result))
-              vim.fn.setreg("+", result)
-            end
-          end)
-        end,
         find_in_dir = function(state)
           local node = state.tree:get_node()
           local path = node:get_id()
@@ -112,17 +65,17 @@ return {
         end,
       },
       window = {
+        position = "left",
         width = 30,
         mappings = {
           ["<space>"] = false, -- disable space until we figure out which-key disabling
-          ["[b"] = "prev_source",
-          ["]b"] = "next_source",
+          o = false,
+
           F = utils.is_available "telescope.nvim" and "find_in_dir" or nil,
-          O = "system_open",
-          Y = "copy_selector",
           h = "parent_or_close",
           l = "child_or_open",
-          o = "open",
+          ["/"] = "fuzzy_finder",
+          C = "set_root",
         },
         fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
           ["<C-j>"] = "move_cursor_down",
@@ -131,8 +84,9 @@ return {
       },
       filesystem = {
         follow_current_file = { enabled = true },
-        hijack_netrw_behavior = "open_current",
-        use_libuv_file_watcher = vim.fn.has "win32" ~= 1,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hijack_netrw_behavior = "open_default",
       },
       event_handlers = {
         {
