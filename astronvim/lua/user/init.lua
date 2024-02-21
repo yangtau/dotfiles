f53244a00667e7bc2cf2f3dee87f5cf1c8d5850c
yangtau <yanggtau@gmail.com>
2024-02-21 09:39:23 +0800

    add astronvim as submodule

--- astronvim/lua/user/init.lua
+++ astronvim/lua/user/init.lua
@@ -1,12 +1,14 @@
 return {
-  options = {
-    opt = {
-      mouse = "",
-      clipboard = "",
-    },
-    g = {
-      autoformat_enabled = false,
-    },
-  },
-  colorscheme = "catppuccin-latte",
+	options = {
+		opt = {
+			mouse = "",
+			clipboard = "",
+		},
+	},
+	colorscheme = "catppuccin-latte",
+	lsp = {
+		formatting = {
+			format_on_save = false, -- enable or disable automatic formatting on save
+		},
+	},
 }
