de5004cb49203c769e3b52f7464d63260646d849
yangtau <yanggtau@gmail.com>
2024-02-21 09:55:36 +0800

    Format astronvim config

--- astronvim/lua/user/init.lua
+++ astronvim/lua/user/init.lua
@@ -1,14 +1,14 @@
 return {
-	options = {
-		opt = {
-			mouse = "",
-			clipboard = "",
-		},
-	},
-	colorscheme = "catppuccin-latte",
-	lsp = {
-		formatting = {
-			format_on_save = false, -- enable or disable automatic formatting on save
-		},
-	},
+  options = {
+    opt = {
+      mouse = "",
+      clipboard = "",
+    },
+  },
+  colorscheme = "catppuccin-latte",
+  lsp = {
+    formatting = {
+      format_on_save = false, -- enable or disable automatic formatting on save
+    },
+  },
 }
