60b7d3d40391efe304cbc2ffbc703f8ac36e276b
yangtau <yanggtau@gmail.com>
2024-02-22 14:12:57 +0800

    Update lang support for nix & lua

--- astronvim/lua/user/plugins/lang.lua
+++ astronvim/lua/user/plugins/lang.lua
@@ -1,44 +1,40 @@
 local utils = require "astronvim.utils"
 return {
-  -- Golang support
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
-      if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
-          "go",
-          "gomod",
-          "gosum",
-          "gowork",
-          "rust",
-          "bash",
-          "sql",
-          "nix",
-        })
-      end
-    end,
-  },
-
-  {
-    "jay-babu/mason-null-ls.nvim",
-    opts = function(_, opts)
       opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
-        "gomodifytags",
-        "gofumpt",
-        "iferr",
-        "impl",
-        "goimports",
-        "staticcheck",
-        "nixpkgs-fmt",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "rust",
+        "bash",
+        "sql",
+        "nix",
+        "json",
+        "yaml",
       })
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
+        -- go
         "gopls",
-        "rnix-lsp",
+        "gomodifytags",
+        "iferr",
+        "impl",
+        "goimports",
+        "staticcheck",
+        -- "gofumpt",
+        -- nix
+        "nixpkgs-fmt",
+        "nil",
+        -- lua
+        "lua-language-server",
+        "stylua",
       })
     end,
   },
