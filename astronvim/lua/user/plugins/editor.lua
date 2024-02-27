925a92851587e4ef38703d1caafb45812df7d99b
yangtau <yanggtau@gmail.com>
2024-02-27 12:02:33 +0800

    Updat codeium keymap

--- astronvim/lua/user/plugins/editor.lua
+++ astronvim/lua/user/plugins/editor.lua
@@ -42,8 +42,8 @@ return {
     "Exafunction/codeium.vim",
     event = "User AstroFile",
     config = function()
-      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
-      -- vim.keymap.set("i", "<c-;>", function() return vim.fn["codeium#CycleCompletions"](0) end, { expr = true })
+      vim.keymap.set("i", "<C-]>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      -- vim.keymap.set("i", "<C-;>", function() return vim.fn["codeium#CycleCompletions"](0) end, { expr = true })
       -- vim.keymap.set("i", "<c-,>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
       -- vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
     end,
