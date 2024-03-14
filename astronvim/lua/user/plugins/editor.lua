5ff4b08eff6b6a8852f1c91cfc6b0cd6ad9ceec9
yangtau <yanggtau@gmail.com>
2024-03-14 17:45:49 +0800

    nvim leetcode

--- astronvim/lua/user/plugins/editor.lua
+++ astronvim/lua/user/plugins/editor.lua
@@ -48,4 +48,115 @@ return {
       -- vim.keymap.set("i", "<c-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
     end,
   },
+  {
+    "kawre/leetcode.nvim",
+    cmd = "Leet",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
+      ---@type string
+      arg = "leetcode.nvim",
+
+      ---@type lc.lang
+      lang = "rust",
+
+      cn = { -- leetcode.cn
+        enabled = true, ---@type boolean
+        translator = true, ---@type boolean
+        translate_problems = true, ---@type boolean
+      },
+
+      ---@type lc.storage
+      storage = {
+        home = vim.env.HOME .. "/Workspaces/rustal/src",
+        cache = vim.fn.stdpath "cache" .. "/leetcode",
+      },
+
+      ---@type table<string, boolean>
+      plugins = {
+        non_standalone = false,
+      },
+
+      ---@type boolean
+      logging = true,
+
+      injector = {}, ---@type table<lc.lang, lc.inject>
+
+      cache = {
+        update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
+      },
+
+      console = {
+        open_on_runcode = true, ---@type boolean
+
+        dir = "row", ---@type lc.direction
+
+        size = { ---@type lc.size
+          width = "90%",
+          height = "75%",
+        },
+
+        result = {
+          size = "60%", ---@type lc.size
+        },
+
+        testcase = {
+          virt_text = true, ---@type boolean
+
+          size = "40%", ---@type lc.size
+        },
+      },
+
+      description = {
+        position = "left", ---@type lc.position
+
+        width = "40%", ---@type lc.size
+
+        show_stats = true, ---@type boolean
+      },
+
+      hooks = {
+        ---@type fun()[]
+        ["enter"] = {},
+
+        ---@type fun(question: lc.ui.Question)[]
+        ["question_enter"] = {},
+
+        ---@type fun()[]
+        ["leave"] = {},
+      },
+
+      keys = {
+        toggle = { "q" }, ---@type string|string[]
+        confirm = { "<CR>" }, ---@type string|string[]
+
+        reset_testcases = "r", ---@type string
+        use_testcase = "U", ---@type string
+        focus_testcases = "H", ---@type string
+        focus_result = "L", ---@type string
+      },
+
+      ---@type lc.highlights
+      theme = {},
+
+      ---@type boolean
+      image_support = false,
+    },
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
