63913a1e32e5e5b52c04666936ab96a92aeaa4b6
yangtau <yanggtau@gmail.com>
2024-04-08 12:00:28 +0800

    add lua style for wezterm config & set font

--- wezterm/configs/keymap.lua
+++ wezterm/configs/keymap.lua
@@ -1,85 +1,85 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 local act = wezterm.action
 
 local M = {}
 
 local function is_vim_or_tmux(pane)
-	local process = pane:get_foreground_process_name()
-	local title = pane:get_title()
-	-- wezterm.log_info("process: ", process)
-	-- wezterm.log_info("title: ", title)
-	return string.find(title, "NVIM")
-		or string.find(title, "tmux")
-		or string.find(process, "nvim")
-		or string.find(process, "tmux")
+  local process = pane:get_foreground_process_name()
+  local title = pane:get_title()
+  -- wezterm.log_info("process: ", process)
+  -- wezterm.log_info("title: ", title)
+  return string.find(title, "NVIM")
+    or string.find(title, "tmux")
+    or string.find(process, "nvim")
+    or string.find(process, "tmux")
 end
 
 local function split_nav(key, resize_or_move, direction)
-	local mod = "CTRL"
-	return {
-		key = key,
-		mods = mod,
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim_or_tmux(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({ SendKey = { key = key, mods = mod } }, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction, 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction }, pane)
-				end
-			end
-		end),
-	}
+  local mod = "CTRL"
+  return {
+    key = key,
+    mods = mod,
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim_or_tmux(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({ SendKey = { key = key, mods = mod } }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction, 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction }, pane)
+        end
+      end
+    end),
+  }
 end
 
 function M.append(config)
-	local options = {
-		disable_default_key_bindings = true,
-		disable_default_mouse_bindings = false,
+  local options = {
+    disable_default_key_bindings = true,
+    disable_default_mouse_bindings = false,
 
-		-- quick_select_alphabet = "colemak", -- default: qwerty
+    -- quick_select_alphabet = "colemak", -- default: qwerty
 
-		-- Keyboard mappings
-		-- timeout_milliseconds defaults to 1000
-		-- leader = { key = "p", mods = "SUPER", timeout_milliseconds = 5000 },
+    -- Keyboard mappings
+    -- timeout_milliseconds defaults to 1000
+    -- leader = { key = "p", mods = "SUPER", timeout_milliseconds = 5000 },
 
-		keys = {
-			{ key = "p", mods = "SUPER", action = act.ActivateCommandPalette },
+    keys = {
+      { key = "p", mods = "SUPER", action = act.ActivateCommandPalette },
 
-			-- copy & paste
-			{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
-			{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+      -- copy & paste
+      { key = "c", mods = "SUPER", action = act.CopyTo "Clipboard" },
+      { key = "v", mods = "SUPER", action = act.PasteFrom "Clipboard" },
 
-			-- tabs
-			{ key = "1", mods = "SUPER", action = act({ ActivateTab = 0 }) },
-			{ key = "2", mods = "SUPER", action = act({ ActivateTab = 1 }) },
-			{ key = "3", mods = "SUPER", action = act({ ActivateTab = 2 }) },
-			{ key = "4", mods = "SUPER", action = act({ ActivateTab = 3 }) },
-			{ key = "5", mods = "SUPER", action = act({ ActivateTab = 4 }) },
-			{ key = "6", mods = "SUPER", action = act({ ActivateTab = 5 }) },
-			{ key = "t", mods = "SUPER", action = act({ SpawnCommandInNewTab = { cwd = os.getenv("HOME") } }) },
-			{ key = "w", mods = "SUPER", action = act({ CloseCurrentTab = { confirm = true } }) },
+      -- tabs
+      { key = "1", mods = "SUPER", action = act { ActivateTab = 0 } },
+      { key = "2", mods = "SUPER", action = act { ActivateTab = 1 } },
+      { key = "3", mods = "SUPER", action = act { ActivateTab = 2 } },
+      { key = "4", mods = "SUPER", action = act { ActivateTab = 3 } },
+      { key = "5", mods = "SUPER", action = act { ActivateTab = 4 } },
+      { key = "6", mods = "SUPER", action = act { ActivateTab = 5 } },
+      { key = "t", mods = "SUPER", action = act { SpawnCommandInNewTab = { cwd = os.getenv "HOME" } } },
+      { key = "w", mods = "SUPER", action = act { CloseCurrentTab = { confirm = true } } },
 
-			-- pane
-			{ key = "z", mods = "SUPER", action = act.TogglePaneZoomState },
-			{ key = "-", mods = "CTRL", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-			{ key = "\\", mods = "CTRL", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-			split_nav("h", "move", "Left"),
-			split_nav("j", "move", "Down"),
-			split_nav("k", "move", "Up"),
-			split_nav("l", "move", "Right"),
-			split_nav("LeftArrow", "resize", "Left"),
-			split_nav("DownArrow", "resize", "Down"),
-			split_nav("UpArrow", "resize", "Up"),
-			split_nav("RightArrow", "resize", "Right"),
-		},
-	}
+      -- pane
+      { key = "z", mods = "SUPER", action = act.TogglePaneZoomState },
+      { key = "-", mods = "CTRL", action = act { SplitVertical = { domain = "CurrentPaneDomain" } } },
+      { key = "\\", mods = "CTRL", action = act { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+      split_nav("h", "move", "Left"),
+      split_nav("j", "move", "Down"),
+      split_nav("k", "move", "Up"),
+      split_nav("l", "move", "Right"),
+      split_nav("LeftArrow", "resize", "Left"),
+      split_nav("DownArrow", "resize", "Down"),
+      split_nav("UpArrow", "resize", "Up"),
+      split_nav("RightArrow", "resize", "Right"),
+    },
+  }
 
-	for key, value in pairs(options) do
-		config[key] = value
-	end
+  for key, value in pairs(options) do
+    config[key] = value
+  end
 end
 
 return M
--- wezterm/configs/ui.lua
+++ wezterm/configs/ui.lua
@@ -1,62 +1,62 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 local M = {}
 
 wezterm.on("format-tab-title", function(tab, _, _, _, _, _)
-	local title = tab.tab_title
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-	else
-		title = " Tab #" .. tab.tab_index + 1 .. " "
-	end
+  local title = tab.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+  else
+    title = " Tab #" .. tab.tab_index + 1 .. " "
+  end
 
-	return { { Text = title } }
+  return { { Text = title } }
 end)
 
 function M.append(config)
-	-- theme
-	local catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
-	catppuccin.background = "#ffffff"
-	catppuccin.tab_bar.background = "#f0f0f0"
-	catppuccin.tab_bar.active_tab.bg_color = "#c2c2df"
-	catppuccin.tab_bar.active_tab.fg_color = "#111111"
-	catppuccin.tab_bar.active_tab.intensity = "Bold"
-	catppuccin.tab_bar.inactive_tab.fg_color = "#7c7f99"
+  -- theme
+  local catppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
+  catppuccin.background = "#ffffff"
+  catppuccin.tab_bar.background = "#f0f0f0"
+  catppuccin.tab_bar.active_tab.bg_color = "#c2c2df"
+  catppuccin.tab_bar.active_tab.fg_color = "#111111"
+  catppuccin.tab_bar.active_tab.intensity = "Bold"
+  catppuccin.tab_bar.inactive_tab.fg_color = "#7c7f99"
 
-	local options = {
-		default_cursor_style = "BlinkingBlock",
-		font_size = 16,
-		font = wezterm.font_with_fallback({ "Firacode Nerd Font Mono" }),
+  local options = {
+    default_cursor_style = "BlinkingBlock",
+    font_size = 16,
+    font = wezterm.font_with_fallback { "Monaco", "Noto Sans Mono CJK SC", "Apple Color Emoji" },
 
-		color_schemes = {
-			["catppuccin"] = catppuccin,
-		},
-		color_scheme = "catppuccin",
+    color_schemes = {
+      ["catppuccin"] = catppuccin,
+    },
+    color_scheme = "catppuccin",
 
-		enable_scroll_bar = true, -- default: false
-		scrollback_lines = 100000, --defauls: 3500
+    enable_scroll_bar = true, -- default: false
+    scrollback_lines = 100000, --defauls: 3500
 
-		use_fancy_tab_bar = false, -- default: true
-		tab_bar_at_bottom = false,
-		hide_tab_bar_if_only_one_tab = true, -- default: false
-		show_new_tab_button_in_tab_bar = false,
-		text_background_opacity = 0.95, -- tab bar opacity
+    use_fancy_tab_bar = false, -- default: true
+    tab_bar_at_bottom = false,
+    hide_tab_bar_if_only_one_tab = true, -- default: false
+    show_new_tab_button_in_tab_bar = false,
+    text_background_opacity = 0.95, -- tab bar opacity
 
-		window_background_opacity = 0.96,
+    window_background_opacity = 0.96,
 
-		inactive_pane_hsb = {
-			saturation = 0.92,
-			brightness = 0.92,
-		},
+    inactive_pane_hsb = {
+      saturation = 0.92,
+      brightness = 0.92,
+    },
 
-		-- Tab bar can't have padding https://github.com/wez/wezterm/issues/3077
-		window_padding = { left = 6, right = 6, top = 6, bottom = 6 },
-		window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
-		macos_window_background_blur = 10,
-	}
+    -- Tab bar can't have padding https://github.com/wez/wezterm/issues/3077
+    window_padding = { left = 6, right = 6, top = 6, bottom = 6 },
+    window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
+    macos_window_background_blur = 10,
+  }
 
-	for key, value in pairs(options) do
-		config[key] = value
-	end
+  for key, value in pairs(options) do
+    config[key] = value
+  end
 end
 
 return M
