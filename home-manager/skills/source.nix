d5226c9f5b630718c1e903956be334d9302cacee
yangtao.tau <yangtao.tau@bytedance.com>
2026-09-02 16:08:59 +0800

    update skills

--- home-manager/skills/source.nix
+++ home-manager/skills/source.nix
@@ -24,66 +24,81 @@
   }
 
   {
-    url = "https://github.com/ShawnPana/smux.git";
+    url = "https://github.com/yangtau/skills.git";
     ref = "refs/heads/main";
-    rev = "f8f591b0b7966b210aa0b68f4f3bce54cf64e07b";
+    rev = "f61733fd9a5bba88aeb288a0aea04ff0747ea95c";
+    sha256 = "1izsvmhcmlh9qsb3l841mk09bdbylzwqpvzcciw5w75clirkrrlw";
     skills = [
-      { dir = "skills/smux"; }
+      { dir = "skills/fix-todos"; }
+      { dir = "skills/simplify"; }
     ];
   }
 
   {
-    url = "https://github.com/mattpocock/skills.git";
-    ref = "refs/heads/main";
-    rev = "6654f6b60cd9d5be8b54c6fafe44346dabeb3b76";
+    url = " https://github.com/ShawnPana/smux.git ";
+    ref = " refs/heads/main ";
+    rev = "
+          f8f591b0b7966b210aa0b68f4f3bce54cf64e07b ";
+    skills = [
+      { dir = " skills/smux "; }
+    ];
+  }
+
+  {
+    url = " https://github.com/mattpocock/skills.git ";
+    ref = " refs/heads/main ";
+    rev = " 6654
+          f6b60cd9d5be8b54c6fafe44346dabeb3b76 ";
     skills = [
       # call by user
-      # { dir = "skills/engineering/ask-matt"; }
-      { dir = "skills/engineering/grill-with-docs"; }
-      # { dir = "skills/engineering/triage"; }
-      { dir = "skills/engineering/improve-codebase-architecture"; }
-      # { dir = "skills/engineering/setup-matt-pocock-skills"; }
-      { dir = "skills/engineering/to-spec"; }
-      # { dir = "skills/engineering/to-tickets"; }
-      # { dir = "skills/engineering/implement"; }
-      # { dir = "skills/engineering/wayfinder"; }
+      # { dir = " skills/engineering/ask-matt "; }
+      { dir = " skills/engineering/grill-with-docs "; }
+      # { dir = " skills/engineering/triage "; }
+      { dir = " skills/engineering/improve-codebase-architecture "; }
+      # { dir = " skills/engineering/setup-matt-pocock-skills "; }
+      { dir = " skills/engineering/to-spec "; }
+      # { dir = " skills/engineering/to-tickets "; }
+      # { dir = " skills/engineering/implement "; }
+      # { dir = " skills/engineering/wayfinder "; }
 
-      { dir = "skills/productivity/grill-me"; }
-      { dir = "skills/productivity/handoff"; }
-      # { dir = "skills/productivity/wait-what"; }
+      { dir = " skills/productivity/grill-me "; }
+      { dir = " skills/productivity/handoff "; }
+      # { dir = " skills/productivity/wait-what "; }
 
       # call by model
-      # { dir = "skills/engineering/prototype"; }
-      # { dir = "skills/engineering/diagnosing-bugs"; }
-      # { dir = "skills/engineering/research"; }
-      { dir = "skills/engineering/tdd"; }
-      { dir = "skills/engineering/domain-modeling"; }
-      { dir = "skills/engineering/codebase-design"; }
-      # { dir = "skills/engineering/code-review"; }
-      { dir = "skills/engineering/resolving-merge-conflicts"; }
-      # { dir = "skills/engineering/wizard"; }
+      # { dir = " skills/engineering/prototype "; }
+      # { dir = " skills/engineering/diagnosing-bugs "; }
+      # { dir = " skills/engineering/research "; }
+      { dir = " skills/engineering/tdd "; }
+      { dir = " skills/engineering/domain-modeling "; }
+      { dir = " skills/engineering/codebase-design "; }
+      # { dir = " skills/engineering/code-review "; }
+      { dir = " skills/engineering/resolving-merge-conflicts "; }
+      # { dir = " skills/engineering/wizard "; }
 
-      { dir = "skills/productivity/grilling"; }
-      { dir = "skills/productivity/writing-for-agents"; }
+      { dir = " skills/productivity/grilling "; }
+      { dir = " skills/productivity/writing-for-agents "; }
     ];
   }
 
   {
-    url = "https://github.com/anthropics/claude-plugins-community.git";
-    ref = "refs/heads/main";
-    rev = "a727be1c7bd6064419b6f60d71993a19198adc17";
+    url = " https://github.com/anthropics/claude-plugins-community.git ";
+    ref = " refs/heads/main ";
+    rev = "
+          a727be1c7bd6064419b6f60d71993a19198adc17 ";
     skills = [
-      { dir = "eli5/skills/eli5"; }
+      { dir = " eli5/skills/eli5 "; }
     ];
   }
 
   {
-    url = "https://github.com/emilkowalski/skills.git";
-    ref = "refs/heads/main";
-    rev = "d23d7f88a2e21c9e4b1418c7abe420f5c1052ba7";
+    url = " https://github.com/emilkowalski/skills.git ";
+    ref = " refs/heads/main ";
+    rev = "
+          d23d7f88a2e21c9e4b1418c7abe420f5c1052ba7 ";
     skills = [
-      # { dir = "skills/emil-design-eng"; }
-      { dir = "skills/apple-design"; }
+      # { dir = " skills/emil-design-eng "; }
+      { dir = " skills/apple-design "; }
     ];
   }
 ]
